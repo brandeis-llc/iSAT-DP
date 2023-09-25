ab0f6ad639fb2888efb3a711d67779045d0ed49d
jxtu <jxtu@brandeis.edu>
2023-09-25 13:38:15 -0400

    Updated data; ingestion code.

---
+++ ingest_dialogue.py
@@ -0,0 +1,69 @@
+import os
+import re
+from typing import Union, List
+from pathlib import Path
+import pandas as pd
+from attrs import define
+
+DATA_FOLDER = Path("data")
+BLOCK_NAME = {"red", "yellow", "green", "blue", "purple", "brown", "mystery"}
+PRONOUNS = {
+    "it",
+    "its",
+    "they",
+    "them",
+    "their",
+    "this",
+    "that",
+    "these",
+    "those",
+}
+
+BLOCK_NAME_PATTERN = rf"{'|'.join(BLOCK_NAME)}"
+PRONOUN_PATTERN = rf"\b({'|'.join(PRONOUNS)})\s"
+
+
+@define
+class Utterance:
+    speaker_id: int
+    start: float
+    end: float
+    text: str  # in lowercase
+
+    @property
+    def is_researcher(self) -> bool:
+        return self.speaker_id == 4
+
+    @property
+    def contain_block_name(self) -> List[str]:
+        return re.findall(BLOCK_NAME_PATTERN, self.text)
+
+    @property
+    def contain_pronouns(self) -> List[str]:
+        return re.findall(PRONOUN_PATTERN, self.text)
+
+
+@define
+class Dialogue:
+    group_id: int
+    utterances: List[Utterance]
+
+
+def read_transcript_csv(csv_file: Union[str, os.PathLike]):
+    csv_df = pd.read_csv(csv_file, header=0)
+    return csv_df
+
+
+def ingest_dialogue(group_id: int) -> Dialogue:
+    csv_file = DATA_FOLDER.joinpath(f"Group_0{group_id}_Oracle.csv")
+    csv_df = read_transcript_csv(csv_file)
+    utterances = []
+    for _, row in csv_df.iterrows():
+        utt = Utterance(row.Participant, row.Start, row.End, row.Transcript.strip().lower())
+        utterances.append(utt)
+    dialogue = Dialogue(group_id, utterances)
+    return dialogue
+
+
+if __name__ == '__main__':
+    ingest_dialogue(1)
